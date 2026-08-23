bae7507b9beec0f14ee74e3e0f8dcf9fbd4d108a
sheetung <30528385+sheetung@users.noreply.github.com>
2026-08-23 17:12:33 +0800

    feat: bootstrap installed plugin workers

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import { App } from "./App";
 import { PetView } from "./PetView";
 import { installPetDomBridge } from "./plugins/dom-bridge";
 import { PluginNavigation } from "./plugins/PluginNavigation";
+import { bootstrapPluginRuntime } from "./plugins/runtime";
 import "./styles.css";
 
 const params = new URLSearchParams(window.location.search);
 const isPetWindow = params.get("window") === "pet";
 if (isPetWindow) document.documentElement.classList.add("pet-window");
-if (isPetWindow) installPetDomBridge();
+if (isPetWindow) {
+  installPetDomBridge();
+} else {
+  bootstrapPluginRuntime();
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
